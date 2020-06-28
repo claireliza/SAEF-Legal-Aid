363b7cb7138deb4c68ffdabf48d29ac7e4051940
AKAlilonu <akalilonu@gmail.com>
2020-06-28 02:14:03 -0500

    did a little styling

--- review.js
+++ review.js
@@ -1,50 +1,44 @@
-function Option() {
-    this.text = "";
-    this.question = "";
-    this.answers = [];
-    this.lastOption = null;
-
-    this.setLastOption = function (option) {
-        this.lastOption = option;
-    };
-
-    this.getLastOption = function () {
-        return this.lastOption;
-    };
-
-    this.addAnswer = function (option) {
-        option.setLastOption(this);
-        this.answers[this.answers.length] = option;
-    };
-
-    this.getAnswers = function () {
-        return this.answers;
-    };
-
-    this.removeAnswers = function () {
-        this.answers = [];
-    };
-}
-
-const surveyArray = [{}];
-
-function makeSurvey(array) {
-    const o = new Option();
-
-    for (let i = 0; i < array.length; i++) {
-        const e = array[i];
-        o.text = e.text;
-        o.question = e.question;
-        for (let j = 0; j < e.answers.length; j++) {
-            const f = [e.answers[j]];
-            o.addAnswer(makeSurvey(f));
-        }
+const reviewSurveyArray = [
+    {
+        question: "How much of this survey were you able to complete based on your previous knowledge of Family Law?",
+        answers: ["100%", "75%", "50%", "25%", "0%"],
+    },
+    {
+        question: "How helpful were the “hover” tools for defining words you may have not known?",
+        answers: ["Very Helpful", "Somewhat Helpful", "Not Very Helpful"],
+    },
+    {
+        question: "How likely are you to reccomend this website? 1: not likely at all ; 5: very likely",
+        answers: ["1", "2", "3", "4", "5"],
+    },
+    {
+        question: "Did you find the relevant referral you needed to help you resolve the legal matter?",
+        answers: ["Yes", "No"],
+    },
+    {
+        question: "On a scale of 1-5 (1: none 5: very much) How much has your knowledge of family law increased from completing the survey?",
+        answers: ["1", "2", "3", "4", "5"],
     }
+];
 
-    return o;
+function toggleThankYou(array, position) {
+    const thankYou = document.getElementById("thank_you");
+    if (position === array.length) {
+        document.getElementById("question").style.display = "none";
+        document.getElementById("buttons").style.display = "none";
+        thankYou.style.display = "initial";
+        thankYou.firstElementChild.style.display = "initial";
+    } else {
+        thankYou.style.display = "none";
+        for (const t of thankYou.children) {
+            t.style.display = "none";
+        }
+        document.getElementById("question").style.display = "intial";
+        document.getElementById("buttons").style.display = "initial";
+    }
 }
 
-function setupBackButton(option) {
+function setupBackButton(array, position) {
     const backButtonArea = document.getElementById("back");
     const backButton = document.getElementById("back_button");
     const newBackButton = document.createElement("input");
@@ -52,45 +46,23 @@ function setupBackButton(option) {
     newBackButton.setAttribute("value", "Back");
     newBackButton.setAttribute("id", "back_button");
 
-    if (option.getLastOption() === null) {
+    if (position === 0) {
         newBackButton.addEventListener("click", function () {
-            window.location.assign("start_survey.html");
+            window.location.assign("survey.html");
         });
     }
     else {
         newBackButton.addEventListener("click", function () {
-            setup(option.getLastOption());
+            setup(array, position - 1);
         });
     }
     backButtonArea.replaceChild(newBackButton, backButton);
 }
 
-function setupProgressBar(option) {
-    function countQuestionsLeft(o, counter) {
-        const answers = o.getAnswers();
-        if (answers.length === 0) {
-            return counter;
-        } else {
-            counter += 1;
-            return Math.max(...answers.map((a) => {
-                return countQuestionsLeft(a, counter);
-            }));
-        }
-    }
-
-    function countQuestionsDone(o, counter) {
-        if (o.getLastOption() === null) {
-            return counter;
-        }
-        else {
-            counter += 1;
-            return countQuestionsDone(o.getLastOption(), counter);
-        }
-    }
-
+function setupProgressBar(array, position) {
     const progressBar = document.getElementById("progress_bar");
-    const questionsLeft = countQuestionsLeft(option, 0);
-    const questionsDone = countQuestionsDone(option, 0);
+    const questionsDone = position;
+    const questionsLeft = reviewSurveyArray.length - questionsDone;
     progressBar.setAttribute("max", `${questionsLeft + questionsDone}`);
     progressBar.setAttribute("value", `${questionsDone}`);
     // SOURCE: https://stackoverflow.com/questions/18269286/shorthand-for-if-else-statement
@@ -101,7 +73,7 @@ function setupProgressBar(option) {
         : `At most ${questionsLeft} questions`} left.`;
 }
 
-function replaceButtons(option) {
+function replaceButtons(array, position) {
     // Remove old buttons
     const buttonArea = document.getElementById("buttons");
     // SOURCE: https://www.geeksforgeeks.org/remove-all-the-child-elements-of-a-dom-node-in-javascript/
@@ -112,14 +84,12 @@ function replaceButtons(option) {
     }
 
     // Create new buttons
-    for (let i = 0; i < option.getAnswers().length; i++) {
-        const answer = option.getAnswers()[i];
-
+    for (a of array[position].answers) {
         const button = document.createElement("input");
         button.setAttribute("type", "submit");
-        button.setAttribute("value", answer.text);
+        button.setAttribute("value", a);
         button.addEventListener("click", function () {
-            setup(answer);
+            setup(array, position + 1);
         }
         );
 
@@ -128,19 +98,22 @@ function replaceButtons(option) {
     }
 }
 
-function setup(option) {
+function setup(array, position) {
+    // Decide whether it's the end of the survey
+    toggleThankYou(array, position)
+
     // Set up back button
-    setupBackButton(option);
+    setupBackButton(array, position);
 
     // Update progress bar
-    setupProgressBar(option);
+    setupProgressBar(array, position);
 
     // Change question
     const q = document.getElementById("question");
-    q.innerHTML = option.question;
+    q.innerHTML = array[position].question;
 
     // Remove old buttons and create new buttons
-    replaceButtons(option);
+    replaceButtons(array, position);
 }
 
-setup(makeSurvey(surveyArray));
+setup(reviewSurveyArray, 0);
